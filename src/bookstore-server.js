89b2bd11fcfbec84f3ed5a7e7af8669f2960eac8
Anoop M D <anoop.md@sinilabs.co.in>
2018-07-19 02:33:35 +0530

    CORS Support

--- src/bookstore-server.js
+++ src/bookstore-server.js
@@ -18,6 +18,18 @@ app.use(express.json());
 
 // Turn on CORS
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  if ('OPTIONS' === req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
 
 // Turn on URL-encoded parser for REST services
 app.use(express.urlencoded({extended: true}));
--- src/oauth-server.js
+++ src/oauth-server.js
@@ -20,6 +20,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(app.oauth.errorHandler());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  if ('OPTIONS' === req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
+
 app.get('/', (req, res) => {
   return res.json({message: "OAuth server is running"});
 });
